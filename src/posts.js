c3238e62a2774256c0b1315980803671e95ed7be
Troy <troy@lionsrock.com>
2023-10-09 16:39:00 -0500

    Setup react app with basic post grab

---
+++ src/posts.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import PocketBase from 'pocketbase';
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const pb = new PocketBase('https://hundreds-table.pockethost.io');
+
+    pb.collection('posts').getList(1, 50)
+      .then(resultList => {
+        setPosts(resultList.items);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.Title}</h2>
+          <p dangerouslySetInnerHTML={{ __html: post.Content }} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
