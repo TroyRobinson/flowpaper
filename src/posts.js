4229f6a0ad961e44f5ee2f9bd6f320a23541218e
Troy <troy@lionsrock.com>
2023-10-11 16:09:47 -0500

    disconnect Pocketbase

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import PocketBase from 'pocketbase';
+// import PocketBase from 'pocketbase';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
